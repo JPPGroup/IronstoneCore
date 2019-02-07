cea79e7f0f637ceb257e196021cb23a4ca5d4e2d
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-02-07 15:17:52 +0000

    Removed boiler plate and added actual test library

---
+++ Ironstone Core Tests/ServiceInterfaces/DataServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Jpp.Ironstone.Core.Tests.ServiceInterfaces
+{
+    [TestFixture]
+    class DataServiceTests
+    {
+        [Test]
+        public void StoreCreation()
+        {
+            Assert.Pass("Test desgined to pass");
+        }
+    }
+}
