ab282e17d43e1b574430f4a101136afc22105464
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-02-20 09:11:00 +0000

    Added UI Styles

--- Ironstone Core UI/Views/About.xaml.cs
+++ Ironstone Core UI/Views/About.xaml.cs
@@ -19,13 +19,21 @@ namespace Jpp.Ironstone.Core.UI.Views
     /// <summary>
     /// Interaction logic for About.xaml
     /// </summary>
-    public partial class About : UserControl
+    public partial class About : HostedUserControl
     {
         public About(AboutViewModel viewModel)
         {
             InitializeComponent();
 
             this.DataContext = viewModel;
+        }
+
+        public override void Show()
+        {
+        }
+
+        public override void Hide()
+        {
         }
     }
 }
