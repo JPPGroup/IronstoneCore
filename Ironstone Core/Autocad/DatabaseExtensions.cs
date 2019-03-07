d631df7cc05649f17a34e6ef8d8be69c6667ec50
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-03-07 21:40:04 +0000

    Added some additional extension methods

--- Ironstone Core/Autocad/DatabaseExtensions.cs
+++ Ironstone Core/Autocad/DatabaseExtensions.cs
@@ -14,9 +14,9 @@ namespace Jpp.Ironstone.Core.Autocad
         public static BlockTableRecord GetModelSpace(this Database currentDatabase)
         {
             Transaction acTrans = currentDatabase.TransactionManager.TopTransaction;
-            if(acTrans == null)
+            if (acTrans == null)
                 throw new TransactionException("No top transaction");
-            
+
             ObjectId modelSpaceId = SymbolUtilityServices.GetBlockModelSpaceId(currentDatabase);
             BlockTableRecord modelSpace = acTrans.GetObject(modelSpaceId, OpenMode.ForRead) as BlockTableRecord;
 
@@ -41,6 +41,25 @@ namespace Jpp.Ironstone.Core.Autocad
             }*/
 
             return acTrans.GetObject(layoutId, OpenMode.ForRead) as Layout;
+        }
+
+        public static void RemoveAllLayouts(this Database currentDatabase)
+        {
+            Transaction acTrans = currentDatabase.TransactionManager.TopTransaction;
+
+            //Remove all sheets
+            ObjectId newLayout = LayoutManager.Current.CreateLayout("Paper");
+            LayoutManager.Current.SetCurrentLayoutId(newLayout);
+            DBDictionary lays =
+                acTrans.GetObject(currentDatabase.LayoutDictionaryId, OpenMode.ForWrite) as DBDictionary;
+            foreach (DBDictionaryEntry item in lays)
+            {
+                string layoutName = item.Key;
+                if (layoutName != "Model" && layoutName != "Paper")
+                {
+                    LayoutManager.Current.DeleteLayout(layoutName);
+                }
+            }
         }
 
         public static LayerTableRecord GetLayer(this Database currentDatabase, string layerId)
@@ -106,6 +125,68 @@ namespace Jpp.Ironstone.Core.Autocad
         public static void RegisterLayer(this Database currentDatabase, LayerInfo layerInfo)
         {
             RegisterLayer(currentDatabase, layerInfo.LayerId, layerInfo.IndexColor, layerInfo.Linetype);
+        }
+
+        public static void PurgeAll(this Database currentDatabase)
+        {
+            Transaction acTrans = currentDatabase.TransactionManager.TopTransaction;
+            bool toBePurged = true;
+
+            while (toBePurged)
+            {
+                // Create the list of objects to "purge"
+                ObjectIdCollection collection = new ObjectIdCollection();
+
+                LayerTable lt = acTrans.GetObject(currentDatabase.LayerTableId, OpenMode.ForRead) as LayerTable;
+                foreach (ObjectId layer in lt)
+                {
+                    collection.Add(layer);
+                }
+
+                LinetypeTable ltt =
+                    acTrans.GetObject(currentDatabase.LinetypeTableId, OpenMode.ForRead) as LinetypeTable;
+                foreach (ObjectId linetype in ltt)
+                {
+                    collection.Add(linetype);
+                }
+
+                TextStyleTable tst =
+                    acTrans.GetObject(currentDatabase.TextStyleTableId, OpenMode.ForRead) as TextStyleTable;
+                foreach (ObjectId text in tst)
+                {
+                    collection.Add(text);
+                }
+
+                BlockTable bt = acTrans.GetObject(currentDatabase.BlockTableId, OpenMode.ForRead) as BlockTable;
+                foreach (ObjectId block in bt)
+                {
+                    collection.Add(block);
+                }
+
+                DBDictionary tsd =
+                    acTrans.GetObject(currentDatabase.TableStyleDictionaryId, OpenMode.ForRead) as DBDictionary;
+                foreach (DBDictionaryEntry ts in tsd)
+                {
+                    collection.Add(ts.Value);
+                }
+
+                // Call the Purge function to filter the list
+                currentDatabase.Purge(collection);
+
+                if (collection.Count > 0)
+                {
+                    // Erase each of the objects we've been allowed to
+                    foreach (ObjectId id in collection)
+                    {
+                        DBObject obj = acTrans.GetObject(id, OpenMode.ForWrite);
+                        obj.Erase();
+                    }
+                }
+                else
+                {
+                    toBePurged = false;
+                }
+            }
         }
     }
 }
---
+++ Ironstone Core/Autocad/EntityExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace Jpp.Ironstone.Core.Autocad
+{
+    public static class EntityExtension
+    {
+        public static void Flatten(this Entity obj)
+        {
+            Transaction acTrans = obj.Database.TransactionManager.TopTransaction;
+
+            //TODO: Is this the right place, add more types?
+
+            if (obj is Polyline)
+            {
+                Polyline pl = obj as Polyline;
+                pl.Elevation = 0;
+            }
+            if (obj is Polyline2d)
+            {
+                Polyline2d pl = obj as Polyline2d;
+                pl.Elevation = 0;
+            }
+            if (obj is Polyline3d)
+            {
+                Polyline3d pl3d = obj as Polyline3d;
+                foreach (ObjectId id in pl3d)
+                {
+                    PolylineVertex3d plv3d = acTrans.GetObject(id, OpenMode.ForWrite) as PolylineVertex3d;
+                    Point3d p3d = plv3d.Position;
+                    plv3d.Position = new Point3d(p3d.X, p3d.Y, 0);
+                }
+            }
+        }
+    }
+}
