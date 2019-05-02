1a80f6baca8f318be053e127e8dfe81424d85890
CEDARBARN\JonA <jon.abbott@jppuk.net>
2019-05-02 09:48:39 +0100

    revised test fixture lib

--- Ironstone Core Tests/IronstoneTestFixture.cs
+++ Ironstone Core Tests/IronstoneTestFixture.cs
@@ -7,12 +7,12 @@ namespace Jpp.Ironstone.Core.Tests
 {
     public abstract class IronstoneTestFixture : BaseNUnitTestFixture
     {
+        private const bool DEBUG = true;
         private const string CORE_LIBRARY = "IronstoneCore.dll";
         public override int ClientTimeout { get; } = 10000;
-        public override bool ShowCommandWindow { get; } = true;
 
-        protected IronstoneTestFixture(Assembly fixtureAssembly, Type fixtureType) : base(fixtureAssembly, fixtureType, CORE_LIBRARY) { }
-        protected IronstoneTestFixture(Assembly fixtureAssembly, Type fixtureType, string drawingFile) : base(fixtureAssembly, fixtureType, drawingFile, CORE_LIBRARY) { }
+        protected IronstoneTestFixture(Assembly fixtureAssembly, Type fixtureType) : base(fixtureAssembly, fixtureType, DEBUG, CORE_LIBRARY) { }
+        protected IronstoneTestFixture(Assembly fixtureAssembly, Type fixtureType, string drawingFile) : base(fixtureAssembly, fixtureType, drawingFile, DEBUG, CORE_LIBRARY) { }
 
         public override void Setup()
         {
