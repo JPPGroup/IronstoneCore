06c90602ec28b91dd8945fdb5e94a5f1712c6ca2
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-05-03 13:59:51 +0100

    Added library for testing CoreUI. Only with a placeholder test at the moment to ensure that the UI library is picked up by code coverage.

---
+++ Ironstone Core UI Tests/Placeholder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Jpp.Ironstone.Core.UI.Tests
+{
+    [TestFixture()]
+    class Placeholder
+    {
+        [Test]
+        public void PlaceholderForCodeCoverage()
+        {
+            Assert.Pass("Valid");
+        }
+    }
+}
