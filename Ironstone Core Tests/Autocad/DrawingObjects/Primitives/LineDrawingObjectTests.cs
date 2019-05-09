39887324bdc94383c432d614388c364cd17dd15b
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-05-09 12:05:55 +0100

    Added placholder check in response to a bug to be fully fleshed out later

---
+++ Ironstone Core Tests/Autocad/DrawingObjects/Primitives/LineDrawingObjectTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Jpp.Ironstone.Core.Tests.Autocad.DrawingObjects.Primitives.Tests
+{
+    [TestFixture()]
+    class LineDrawingObjectTests : IronstoneTestFixture
+    {
+        public LineDrawingObjectTests() : base(Assembly.GetExecutingAssembly(), typeof(LineDrawingObjectTests)) { }
+
+        [Test]
+        public void VerifyIncorrectType()
+        {
+            Assert.IsTrue(RunTest<bool>(nameof(VerifyIncorrectType)));
+        }
+
+        public bool VerifyIncorrectTypeResident()
+        {
+            //Polyline pline = new Polyline();
+            return false;
+        }
+    }
+}
