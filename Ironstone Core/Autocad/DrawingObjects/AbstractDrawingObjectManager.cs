a0c000f3bb89236a921ec3b2a2c3294fbe294825
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-05-09 22:17:03 +0100

    Added extra comments to the Drawing Object Manager interface, and reworked the abstract implementation to include more boilerplatecode.

--- Ironstone Core/Autocad/DrawingObjects/AbstractDrawingObjectManager.cs
+++ Ironstone Core/Autocad/DrawingObjects/AbstractDrawingObjectManager.cs
@@ -5,31 +5,98 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
 
 namespace Jpp.Ironstone.Core.Autocad
 {
-    public abstract class AbstractDrawingObjectManager : IDrawingObjectManager
+    /// <summary>
+    /// Abstract manager implementation for Drawing Objects.
+    /// </summary>
+    /// <typeparam name="T">Class that extends DrawingObject</typeparam>
+    public abstract class AbstractDrawingObjectManager<T> : IDrawingObjectManager where T : DrawingObject
     {
+        public List<T> ManagedObjects;
+
         [XmlIgnore]
         public Document HostDocument { get; set; }
 
+        /// <summary>
+        /// Create an instance of the manager
+        /// </summary>
+        /// <param name="document">The document in which the manager resides</param>
         public AbstractDrawingObjectManager(Document document)
         {
             HostDocument = document;
+            ManagedObjects = new List<T>();
         }
 
-        public AbstractDrawingObjectManager()
+        protected AbstractDrawingObjectManager()
         {
         }
 
-        public abstract void UpdateDirty();
+        public virtual void UpdateDirty()
+        {
+            RemoveErased();
+        }
 
         public abstract void UpdateAll();
 
         public abstract void Clear();
 
-        public abstract void AllDirty();
+        public virtual void AllDirty()
+        {
+            foreach (T managedObject in ManagedObjects)
+            {
+                managedObject.DirtyModified = true;
+            }
+        }
 
-        public abstract void ActivateObjects();
+        public virtual void ActivateObjects()
+        {
+            // Get the current document and database
+            Database acCurDb = HostDocument.Database;
+
+            //TODO: Trace usage paths to confirm transaction is needed
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                foreach (T managedObject in ManagedObjects)
+                {
+                    managedObject.CreateActiveObject();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add new object to managed object collection
+        /// </summary>
+        /// <param name="toBeManaged">Object to be added. Dirty added does not need to be marked</param>
+        public void Add(T toBeManaged)
+        {
+            ManagedObjects.Add(toBeManaged);
+            toBeManaged.DirtyAdded = true;
+        }
+
+        /// <summary>
+        /// Method to iterate over managed objects and remove any that have been deleted
+        /// </summary>
+        public void RemoveErased()
+        {
+            List<int> indicesToRemove = new List<int>();
+
+            for (int i = ManagedObjects.Count - 1; i >= 0; i--)
+            {
+                if (ManagedObjects[i].DirtyRemoved)
+                {
+                    indicesToRemove.Add(i);
+                }
+            }
+
+            indicesToRemove.Reverse();
+
+            foreach (int i in indicesToRemove)
+            {
+                ManagedObjects.RemoveAt(i);
+            }
+        }
     }
 }
--- Ironstone Core/Autocad/DrawingObjects/IDrawingObjectManager.cs
+++ Ironstone Core/Autocad/DrawingObjects/IDrawingObjectManager.cs
@@ -8,14 +8,29 @@ namespace Jpp.Ironstone.Core.Autocad
 {
     public interface IDrawingObjectManager
     {
+        /// <summary>
+        /// Update any objects marked as dirty. Called by the framework
+        /// </summary>
         void UpdateDirty();
 
+        /// <summary>
+        /// Update all objects. Called by the framework
+        /// </summary>
         void UpdateAll();
 
+        /// <summary>
+        /// Removed all objects managed by this instance
+        /// </summary>
         void Clear();
 
+        /// <summary>
+        /// Mark all objects managed by this instance as dirty
+        /// </summary>
         void AllDirty();
 
+        /// <summary>
+        /// Activate all objects managed by this instance. Called by the framework upon manager being loaded.
+        /// </summary>
         void ActivateObjects();
     }
 }
