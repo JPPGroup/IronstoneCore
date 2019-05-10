950cdf180dcab689ef7b284cb2e87356774934c2
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-05-10 11:57:08 +0100

    Added offset function to curve class to create offset objects at a specified distance and side

--- Ironstone Core/Autocad/DrawingObjects/Primitives/CurveDrawingObject.cs
+++ Ironstone Core/Autocad/DrawingObjects/Primitives/CurveDrawingObject.cs
@@ -41,11 +41,34 @@ namespace Jpp.Ironstone.Core.Autocad
         {
             get
             {
-                return 0;
+                Transaction acTrans = Application.DocumentManager.MdiActiveDocument.TransactionManager.TopTransaction;
+                Point3d result;
+                using (Curve l = acTrans.GetObject(BaseObject, OpenMode.ForWrite) as Curve)
+                {
+                    switch (l)
+                    {
+                        case Line line:
+                            return line.Angle;
+                            
+                        case Polyline polyline:
+                            //TODO: Move to generic extension
+                            Point3d start3d = polyline.GetPoint3dAt(0);
+                            Point3d end3d = polyline.GetPoint3dAt(1);
+
+                            Point3d start = new Point3d(start3d.X, start3d.Y, 0);
+                            Point3d end = new Point3d(end3d.X, end3d.Y, 0);
+                            Vector3d lineVector = start.GetVectorTo(end);
+                            return lineVector.GetAngleTo(Vector3d.XAxis, Vector3d.ZAxis);
+
+                            
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
             }
             set
             {
-                return;
+                throw new NotImplementedException();
             }
         }
 
@@ -92,5 +115,65 @@ namespace Jpp.Ironstone.Core.Autocad
                 }
             }
         }
+
+        protected Curve CreateLeftOffset(double offsetDistance)
+        {
+            return CreateOffset(offsetDistance, Side.Left);
+        }
+
+        protected Curve CreateRightOffset(double offsetDistance)
+        {
+            return CreateOffset(offsetDistance, Side.Right);
+        }
+
+        protected Curve CreateOffset(double offsetDistance, Side side)
+        {
+            Transaction acTrans = Application.DocumentManager.MdiActiveDocument.TransactionManager.TopTransaction;
+
+            Curve baseEntity = acTrans.GetObject(this.BaseObject, OpenMode.ForRead) as Curve;
+
+            Curve positiveOffset = baseEntity.GetOffsetCurves(offsetDistance)[0] as Curve;
+            Curve negativeOffset = baseEntity.GetOffsetCurves(-offsetDistance)[0] as Curve;
+
+            Point2d start = new Point2d(baseEntity.StartPoint.X, baseEntity.StartPoint.Y);
+            var offSetVector = start.GetVectorTo(new Point2d(positiveOffset.StartPoint.X, positiveOffset.StartPoint.Y));
+            double offsetAngle;
+            switch (side)
+            {
+                case Side.Left:
+                    offsetAngle = Rotation + Math.PI / 2;
+                    while (offsetAngle >= Math.PI * 2)
+                    {
+                        offsetAngle -= 2 * Math.PI;
+                    }
+                    break;
+
+                case Side.Right:
+                    offsetAngle = Rotation - Math.PI / 2;
+                    while (offsetAngle < 0)
+                    {
+                        offsetAngle += 2 * Math.PI;
+                    }
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(side), "Invalid enum");
+
+            }
+            if (Math.Abs(offsetAngle - offSetVector.Angle) < Tolerance.Global.GetAngle())
+            {
+                return positiveOffset;
+            }
+            else
+            {
+                return negativeOffset;
+            }
+        }
+    }
+
+    public enum Side
+    {
+        Left,
+        Right
     }
 }
