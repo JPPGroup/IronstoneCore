4d8851b79e681195587733a0346113d0c81a9510
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-12-18 21:56:38 +0000

    Unit tests added. Wont compile until test package reference is updated.

---
+++ Ironstone Core Tests/IronstoneCommandAttributeTests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Reflection;
+using Autodesk.AutoCAD.Runtime;
+using NUnit.Framework;
+
+namespace Jpp.Ironstone.Core.Tests
+{
+    [TestFixture]
+    class IronstoneCommandAttributeTests : IronstoneTestFixture
+    {
+        public IronstoneCommandAttributeTests() : base(Assembly.GetExecutingAssembly(), typeof(IronstoneCommandAttributeTests)) { }
+
+        [Test]
+        public void LoggerCalled()
+        {
+            bool result = RunTest<bool>(nameof(LoggerCalledResident));
+
+            Configuration config = new Configuration();
+            config.TestSettings();
+            string contents;
+
+            using (TextReader tr = File.OpenText(config.LogFile))
+            {
+                contents = tr.ReadToEnd();
+            }
+
+            StringAssert.Contains("LoggerCalledResident", contents);
+        }
+
+        [IronstoneCommand]
+        [CommandMethod("LoggerCalledResident")]
+        public bool LoggerCalledResident()
+        {
+            return true;
+        }
+    }
+}
