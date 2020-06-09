2fa5de4904e8c4a2d9a3f3bd0b746711b17fcb6a
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-06-09 22:24:41 +0100

    Added extensions to allow for getting the elevation of a feature line at any point - requires testing

---
+++ Ironstone Core/Autocad/FeatureLineExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.Civil;
+using Autodesk.Civil.DatabaseServices;
+
+namespace Jpp.Ironstone.Core.Autocad
+{
+    public static class FeatureLineExtensions
+    {
+        public static double GetElevationAtPoint(this FeatureLine featureLine, Point3d point)
+        {
+            Point3dCollection points = featureLine.GetPoints(FeatureLinePointType.AllPoints);
+
+            //Se if point already exists
+            foreach (Point3d point3d in points)
+            {
+                if (point3d.X == point.X && point3d.Y == point.Y)
+                {
+                    return point3d.Z;
+                }
+            }
+
+            double targetParam = featureLine.GetParameterAtPoint(point);
+            Point3d lastPoint = Point3d.Origin;
+
+            //if not find closest point and interpolate between
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point3d testPoint = points[i];
+                double param = featureLine.GetParameterAtPoint(testPoint);
+                if(targetParam < param)
+                    break;
+
+                lastPoint = testPoint;
+            }
+
+            double additionalDistance = Math.Sqrt(Math.Pow(point.X - lastPoint.X, 2) + Math.Pow(point.Y - lastPoint.Y, 2));
+            double grade = featureLine.GetGradeOutAtPoint(lastPoint);
+            return (lastPoint.Z + grade * additionalDistance);
+        }
+    }
+}
