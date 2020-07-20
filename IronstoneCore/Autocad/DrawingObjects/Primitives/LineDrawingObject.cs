526f1b891ed5ae2d362423fc599de8a403d2e735
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-07-20 17:29:37 +0100

    Added a number of helper methods to determine relations of lines, along with unit tests.

--- IronstoneCore/Autocad/DrawingObjects/Primitives/LineDrawingObject.cs
+++ IronstoneCore/Autocad/DrawingObjects/Primitives/LineDrawingObject.cs
@@ -171,6 +171,88 @@ namespace Jpp.Ironstone.Core.Autocad
             }
         }
 
+        public bool IsTargetSegmentOf(LineDrawingObject target)
+        {
+            if (!DoesTargetHaveSameGradient(target))
+                return false;
+
+            if (IsTargetExtensionOf(target))
+                return false;
+
+            if (IsPointOnLine(new Point2d(target.StartPoint.X, target.StartPoint.Y)) || IsPointOnLine(new Point2d(target.EndPoint.X, target.EndPoint.Y)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsTargetExtensionOf(LineDrawingObject target)
+        {
+            if (!DoesTargetHaveSameGradient(target))
+                return false;
+
+
+            if (target.StartPoint == StartPoint)
+            {
+                //Check other point is not on line
+                return !IsPointOnLine(new Point2d(target.EndPoint.X, target.EndPoint.Y));
+            }
+            if (target.EndPoint == StartPoint)
+            {
+                //Check other point is not on line
+                return !IsPointOnLine(new Point2d(target.StartPoint.X, target.StartPoint.Y));
+            }
+
+            if (target.StartPoint == EndPoint)
+            {
+                //Check other point is not on line
+                return !IsPointOnLine(new Point2d(target.EndPoint.X, target.EndPoint.Y));
+            }
+            if (target.EndPoint == EndPoint)
+            {
+                //Check other point is not on line
+                return !IsPointOnLine(new Point2d(target.StartPoint.X, target.StartPoint.Y));
+            }
+
+            return false;
+        }
+
+        private bool DoesTargetHaveSameGradient(LineDrawingObject target)
+        {
+            double thisGradient = (this.EndPoint.Y - this.StartPoint.Y) / (this.EndPoint.X - this.StartPoint.X);
+            double targetGradient = (target.EndPoint.Y - target.StartPoint.Y) / (target.EndPoint.X - target.StartPoint.X);
+
+
+            //TODO: CHeck this value is ok
+            bool bothInfinite = double.IsInfinity(thisGradient) && double.IsInfinity(targetGradient);
+
+            if (Math.Abs(thisGradient - targetGradient) > 0.001 && !bothInfinite)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsPointOnLine(Point2d point)
+        {
+            //Get vector from start to point, and from point to end. If both vectors summed equal the total vector from start to end, its on the line
+            double pointx = point.X - StartPoint.X;
+            double pointy = point.Y - StartPoint.Y;
+            double distA = Math.Sqrt(Math.Pow(pointx, 2) + Math.Pow(pointy, 2));
+
+            double endx = EndPoint.X - point.X;
+            double endy = EndPoint.Y - point.Y;
+            double distB = Math.Sqrt(Math.Pow(endx, 2) + Math.Pow(endy, 2));
+
+            double overallX = EndPoint.X - StartPoint.X;
+            double overallY = EndPoint.Y - StartPoint.Y;
+            double distC = Math.Sqrt(Math.Pow(overallX, 2) + Math.Pow(overallY, 2));
+
+            return (Math.Abs(distC - (distA + distB)) < 0.001);
+        }
+
         public static LineDrawingObject Create(Database target, Point3d startPoint, Point3d endPoint)
         {
             LineDrawingObject newLineDrawingObject = new LineDrawingObject();
---
+++ IronstoneCoreTests/Autocad/DrawingObjects/Primitives/LineDrawingObjectTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Jpp.Ironstone.Core.Autocad;
+using NUnit.Framework;
+
+namespace Jpp.Ironstone.Core.Tests.Autocad.DrawingObjects.Primitives
+{
+    class LineDrawingObjectTests : IronstoneTestFixture
+    {
+        public LineDrawingObjectTests() : base(Assembly.GetExecutingAssembly(), typeof(LineDrawingObjectTests))
+        {
+        }
+        
+        [TestCase(50, 50, ExpectedResult = true)]
+        [TestCase(100, 100, ExpectedResult = true)]
+        [TestCase(0, 0, ExpectedResult = true)]
+        [TestCase(500, 500, ExpectedResult = false)]
+        [TestCase(-10, -10, ExpectedResult = false)]
+        [TestCase(-150, 10, ExpectedResult = false)]
+        public bool VerifyIsPointOnLine(double x, double y)
+        {
+            Point testPoint = new Point() {X = x, Y = y};
+            return RunTest<bool>(nameof(VerifyIsPointOnLineResident), testPoint);
+        }
+
+        public bool VerifyIsPointOnLineResident(Point testPoint)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            using (Transaction trans = doc.TransactionManager.StartTransaction())
+            {
+                LineDrawingObject line1 = LineDrawingObject.Create(doc.Database, Point3d.Origin, new Point3d(100, 100, 0));
+                Point2d point = new Point2d(testPoint.X, testPoint.Y);
+                return line1.IsPointOnLine(point);
+            }
+        }
+
+        //TODO: Add more tests, dont think all cases are covered
+        [TestCase(25, 25, 50, 50, ExpectedResult = true)]
+        [TestCase(50, 50, 0, 0, ExpectedResult = true)] //Verify inverse gradient is treated the same
+        [TestCase(125, 125, 150, 150, ExpectedResult = false)]
+        [TestCase(100, 100, 200, 200, ExpectedResult = false)] //Check not a continuation
+        [TestCase(-100, -100, 0, 0, ExpectedResult = false)] //Check not before
+        public bool VerifyIsTargetSegmentOf(double startX, double startY, double endX, double endY)
+        {
+            Point start = new Point() {X = startX, Y = startY};
+            Point end = new Point() {X = endX, Y = endY};
+            Line line = new Line() {Start = start, End = end};
+            return RunTest<bool>(nameof(VerifyIsTargetSegmentOfResident), line);
+        }
+
+        //TODO: Add more tests, dont think all cases are covered
+        [TestCase(0, 0, 0, 50, ExpectedResult = true)]
+        [TestCase(0, 150, 0, 250, ExpectedResult = false)]
+        public bool VerifyIsTargetSegmentOfInfinte(double startX, double startY, double endX, double endY)
+        {
+            Point start = new Point() {X = startX, Y = startY};
+            Point end = new Point() {X = endX, Y = endY};
+            Line line = new Line() {Start = start, End = end};
+            return RunTest<bool>(nameof(VerifyIsTargetSegmentOfInfiniteResident), line);
+        }
+
+        public bool VerifyIsTargetSegmentOfResident(Line line)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            using (Transaction trans = doc.TransactionManager.StartTransaction())
+            {
+                //Debugger.Launch();
+                LineDrawingObject subject = LineDrawingObject.Create(doc.Database, Point3d.Origin, new Point3d(100, 100, 0));
+                LineDrawingObject target = LineDrawingObject.Create(doc.Database, new Point3d(line.Start.X, line.Start.Y, 0), new Point3d(line.End.X, line.End.Y, 0));
+
+                return subject.IsTargetSegmentOf(target);
+            }
+        }
+
+        public bool VerifyIsTargetSegmentOfInfiniteResident(Line line)
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            using (Transaction trans = doc.TransactionManager.StartTransaction())
+            {
+                LineDrawingObject subject = LineDrawingObject.Create(doc.Database, Point3d.Origin, new Point3d(0, 100, 0));
+                LineDrawingObject target = LineDrawingObject.Create(doc.Database, new Point3d(line.Start.X, line.Start.Y, 0), new Point3d(line.End.X, line.End.Y, 0));
+
+                return subject.IsTargetSegmentOf(target);
+            }
+        }
+
+        [Serializable]
+        public struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+
+        [Serializable]
+        public struct Line
+        {
+            public Point Start { get; set; }
+            public Point End { get; set; }
+        }
+    }
+}
+
