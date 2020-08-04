e055bbfc28abfd1fba2b4df556b60df8667a7404
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-08-04 10:39:00 +0100

    Ported existing PoC selenium tests to form basepoint for further work

---
+++ IronstoneCoreUI.UITests/RibbonTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace IronstoneCore.UI.UiTests
+{
+    [TestFixture]
+    class RibbonTests
+    {
+        [Test]
+        public void CheckTabs()
+        {
+            var conceptTab = Setup.CurrentDriver.FindElementByName("Ironstone Concept");
+            var designTab = Setup.CurrentDriver.FindElementByName("Ironstone Design");
+
+            Assert.IsNotNull(conceptTab);
+            Assert.IsNotNull(designTab);
+        }
+    }
+}
